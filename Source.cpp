4adbb3348d51b34a3e5f4baa783514f6dbdc098c
Vlad Tatarinskiy <ghoust187@gmail.com>
2024-08-22 16:59:30 +0300

    ы

--- Source.cpp
+++ Source.cpp
@@ -1,76 +1,120 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <algorithm>
+#include <stdexcept>
 
-class Big_integer {
+class big_integer {
 private:
-    std::vector<int> digits;
+    std::string number;
 
-public:
-    Big_integer() {
-
-
-    }
-    Big_integer(const std::string& s) {
-        for (int i = s.length() - 1; i >= 0; --i) {
-            digits.push_back(s[i] - '0');
+   
+    void remove_leading_zeros() {
+        while (number.length() > 1 && number[0] == '0') {
+            number.erase(0, 1);
+        }
+        if (number.length() == 1 && number[0] == '0') {
+            number = "0"; 
         }
     }
 
+public:
+  
+    big_integer(const std::string& num) : number(num) {
+        remove_leading_zeros();
+    }
 
-    Big_integer(Big_integer&& other) noexcept : digits(std::move(other.digits)) {
-
+    
+    big_integer(big_integer&& other) noexcept : number(std::move(other.number)) {
+        other.number.clear(); 
     }
 
 
-    Big_integer& operator=(Big_integer&& other) noexcept {
+    big_integer& operator=(big_integer&& other) noexcept {
         if (this != &other) {
-            digits = std::move(other.digits);
+            number = std::move(other.number);
+            other.number.clear(); 
         }
         return *this;
     }
 
+ 
+    big_integer operator+(const big_integer& other) const {
+        std::string a = number;
+        std::string b = other.number;
 
-    Big_integer operator+(const Big_integer& other) const {
-        Big_integer result;
-        int carry = 0;
-        int i = 0;
-
-        while (i < digits.size() || i < other.digits.size() || carry) {
-            int sum = carry;
-            if (i < digits.size()) {
-                sum += digits[i];
-            }
-            if (i < other.digits.size()) {
-                sum += other.digits[i];
-            }
-            carry = sum / 10;
-            result.digits.push_back(sum % 10);
-            ++i;
+  
+        if (a.length() < b.length()) {
+            std::swap(a, b);
         }
 
-        std::reverse(result.digits.begin(), result.digits.end());
-        return result;
+      
+        std::reverse(a.begin(), a.end());
+        std::reverse(b.begin(), b.end());
+
+        std::string result;
+        int carry = 0;
+
+        for (size_t i = 0; i < a.length(); ++i) {
+            int digitA = a[i] - '0';
+            int digitB = (i < b.length()) ? (b[i] - '0') : 0;
+            int sum = digitA + digitB + carry;
+
+            result.push_back((sum % 10) + '0'); 
+            carry = sum / 10; 
+        }
+
+       
+        if (carry) {
+            result.push_back(carry + '0');
+        }
+
+        std::reverse(result.begin(), result.end()); 
+        return big_integer(result);
     }
 
+ 
+    big_integer operator*(int multiplier) const {
+        if (multiplier == 0) return big_integer("0");
 
-    friend std::ostream& operator<<(std::ostream& os, const Big_integer& bi) {
-        for (int i = 0; i < bi.digits.size(); ++i) {
-            os << bi.digits[i];
+        std::string result;
+        int carry = 0;
+        int n = number.length();
+
+        for (int i = n - 1; i >= 0; --i) {
+            int digit = number[i] - '0';
+            int product = digit * multiplier + carry;
+
+            result.push_back((product % 10) + '0'); 
+            carry = product / 10; 
         }
+
+        
+        while (carry) {
+            result.push_back((carry % 10) + '0');
+            carry /= 10;
+        }
+
+      
+        std::reverse(result.begin(), result.end());
+        return big_integer(result);
+    }
+
+   
+    friend std::ostream& operator<<(std::ostream& os, const big_integer& num) {
+        os << num.number;
         return os;
     }
 };
 
 int main() {
-    Big_integer number1("114575");
-    Big_integer number2("78524");
+    auto number1 = big_integer("114575");
+    auto number2 = big_integer("78524");
+    auto result = number1 + number2;
+    std::cout << result << std::endl; // 193099
 
-    Big_integer number3 = std::move(number1);
-
-    Big_integer result = number3 + number2;
-    std::cout << result << std::endl;
+    auto num3 = big_integer("5555");
+    auto num4 = num3 * 4;
+    std::cout << num4 << std::endl; 
 
     return 0;
 }
